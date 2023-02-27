9c5a39c1adf588e18e8efd4131eb2c4fb39a59b2
BaconPardner <58827769+BaconPardner@users.noreply.github.com>
2023-02-27 23:32:04 +0100

    add tailwind

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
   return (
-    <div>
-      <h1>hi</h1>
+    <div className="container mx-auto">
+      <h1 className="text-7xl font-bold text-center py-10 bg-blue-100">
+        Hello world!
+      </h1>
     </div>
   );
 }
