08378f0f697ea70ef2b2463e2b8e2c63ec97f22b
BaconPardner <58827769+BaconPardner@users.noreply.github.com>
2023-03-01 22:47:32 +0100

    add initial styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,35 @@
+import Home from "./components/home";
+import Navbar from "./components/navbar";
+
+const categories = [
+  "CPU",
+  "Memory",
+  "Motherboards",
+  "GPU",
+  "Computer Case",
+  "Power Supplies",
+  "Fans & Cooling",
+  "Miscellaneous",
+];
+
 function App() {
   return (
-    <div className="container mx-auto">
-      <h1 className="text-7xl font-bold text-center py-10 bg-blue-100">
-        Hello world!
-      </h1>
+    <div className="min-h-screen">
+      <Navbar />
+      <div className="container mx-auto py-8 px-2 flex">
+        <ul className="space-y-2 text-sm pr-10">
+          {categories.map((item, idx) => (
+            <li
+              key={idx}
+              className="py-2 px-4 hover:bg-zinc-100 group cursor-pointer rounded-3xl w-48"
+            >
+              <span className="px-1 mr-2 rounded-3xl bg-zinc-100 group-hover:bg-blue-500 group-hover:py-1 transition-all duration-200 group-hover:text-blue-500" />
+              {item}
+            </li>
+          ))}
+        </ul>
+        <Home />
+      </div>
     </div>
   );
 }
---
+++ client/src/components/home/index.tsx
@@ -0,0 +1,12 @@
+const index = () => {
+  return (
+    <div className="grow">
+      <header className="bg-gray-100 py-40 rounded-3xl text-center uppercase text-gray-500 border">
+        Build PC
+      </header>
+      <section className="py-5">Products goes here</section>
+    </div>
+  );
+};
+
+export default index;
